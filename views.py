7fb0131d9cb497884a32b7110493bd9408689faf
Shaural Patel <patel675@purdue.edu>
2016-10-16 05:45:00 -0400

    Remove uploader and add weather humidity

--- views.py
+++ views.py
@@ -20,6 +20,7 @@ def index():
 def test():
 	headers = {'Content-Type': 'application/json','Ocp-Apim-Subscription-Key': 'a2219199d5834848a8e0546e2645a870'}
 	link = request.form['link']
+	print(link)
 	params = urllib.urlencode({'visualFeatures': 'Tags'})
 	conn = httplib.HTTPSConnection('api.projectoxford.ai')
 	conn.request("POST", "/vision/v1.0/analyze?%s" % params, "{'url':'"+link+"'}", headers)
@@ -42,15 +43,16 @@ def test():
 	resp = requests.get(url)
 	data1 = resp.json()
 	prob = data1['daily']['data'][0]['precipProbability']
+	humidity = data1['daily']['data'][0]['humidity']
 	if prob > 0.75:
 		ans = "No"
 	else:
 		ans = "Yes"
-	return render_template("index.html", name = name, prob = prob, ans = ans)
+	return render_template("index.html", name=name, prob=prob, water=ans, humidity=humidity)
 
-@app.route('/photo.jpg')
-def returnPic():
-	return send_file('photo.jpg')
+# @app.route('/photo.jpg')
+# def returnPic():
+# 	return send_file('photo.jpg')
 
 # @app.route('/onClick1', methods=['POST'])
 # def save_uploaded_file ():
@@ -59,34 +61,35 @@ def returnPic():
 # 		f.save("photo.jpg")
 
 # 	# headers = {'Content-Type': 'application/json','Ocp-Apim-Subscription-Key': 'a2219199d5834848a8e0546e2645a870'}
-	# link = "https://aquaflora.herokuapp.com/photo.jpg"
-	# params = urllib.urlencode({'visualFeatures': 'Tags'})
-	# conn = httplib.HTTPSConnection('https://api.projectoxford.ai')
-	# conn.request("POST", "/vision/v1.0/analyze?%s" % params, "{'url':'"+link+"'}", headers)
-	# response = conn.getresponse()
-	# data = response.read()
-	# json_data = json.loads(data)
-	# names = [r['name'] for r in json_data['tags']]
-	# words_you_dont_want = ['flower', 'plant']
-	# name = ''
-	# for n in names:
-	# 	if n not in words_you_dont_want:
-	# 		name += "%s " % n.title()
+# 	link = "https://aquaflora.herokuapp.com/photo.jpg"
+# 	print(link)
+# 	params = urllib.urlencode({'visualFeatures': 'Tags'})
+# 	conn = httplib.HTTPSConnection('https://api.projectoxford.ai')
+# 	conn.request("POST", "/vision/v1.0/analyze?%s" % params, "{'url':'"+link+"'}", headers)
+# 	response = conn.getresponse()
+# 	data = response.read()
+# 	json_data = json.loads(data)
+# 	names = [r['name'] for r in json_data['tags']]
+# 	words_you_dont_want = ['flower', 'plant']
+# 	name = ''
+# 	for n in names:
+# 		if n not in words_you_dont_want:
+# 			name += "%s " % n.title()
 
-	# if name == '':
-	# 	name = names[0].title()
-	# ftemp = ''
-	# print(name)
-	# conn.close()
-	# url = "https://api.darksky.net/forecast/209bb716d364854504482e794db7bad8/37.8267,-122.4233"
-	# resp = requests.get(url)
-	# data1 = resp.json()
-	# prob = data1['daily']['data'][0]['precipProbability']
-	# if prob > 0.75:
-	# 	ans = "No"
-	# else:
-	# 	ans = "Yes"
-	return render_template("index.html")
+# 	if name == '':
+# 		name = names[0].title()
+# 	ftemp = ''
+# 	print(name)
+# 	conn.close()
+# 	url = "https://api.darksky.net/forecast/209bb716d364854504482e794db7bad8/37.8267,-122.4233"
+# 	resp = requests.get(url)
+# 	data1 = resp.json()
+# 	prob = data1['daily']['data'][0]['precipProbability']
+# 	if prob > 0.75:
+# 		ans = "No"
+# 	else:
+# 		ans = "Yes"
+# 	return render_template("index.html")
 
 #app.run()
 if __name__ == '__main__':
