3eae61a592f4a9fa8b182ab053eabf0b265276ec
Shaural Patel <patel675@purdue.edu>
2016-10-16 05:34:01 -0400

    heroic run

--- views.py
+++ views.py
@@ -87,8 +87,8 @@ def returnPic():
 	# else:
 	# 	ans = "Yes"
 	return render_template("index.html")
-	
-app.run()
-# if __name__ == '__main__':
-#     port = int(os.environ.get('PORT', 5000))
-#     app.run(host='0.0.0.0', port=port)
+
+#app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
